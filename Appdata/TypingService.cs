4de2b44bbcd390d9e7f1cc09af35f4a6fa22fee6
Панин.Владимир <v9250578092@gmail.com>
2025-03-17 10:39:46 +0300

    Считывание клавиш

--- Appdata/TypingService.cs
+++ Appdata/TypingService.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
 
 namespace FastType.Appdata
 {
-    internal class TypingService
+    public class TypingService
     {
+        //Создаем поля для хранения и использования элементов управления в рамках класса
+        private Grid _keyboardGrid;
+        private TextBox _typingTextBox;
+        private TextBlock _typingTextBlock;
+
+        //Создаем конструктор класса для ПРИЕМА элементов управления из интефейса(TypingTutorPage.xaml)
+        public TypingService(Grid keyboardGrid, TextBox typingTextBox, TextBlock typingTextBlock)
+        {
+            _keyboardGrid = keyboardGrid;
+            _typingTextBox = typingTextBox;
+            _typingTextBlock = typingTextBlock;
+
+
+            _typingTextBox.PreviewKeyDown += _typingTextBox_PreviewKeyDown;
+            _typingTextBox.PreviewKeyUp += _typingTextBox_PreviewKeyUp;
+        }
+
+        private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void _typingTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            _typingTextBox.Clear();
+            _typingTextBox.Text = e.Key.ToString();
+        }
     }
 }
