7b4e2e4adcc5b48621db1d90dc638d6ba907274f
Панин.Владимир <v9250578092@gmail.com>
2025-03-20 15:34:04 +0300

    123

--- Appdata/TypingService.cs
+++ Appdata/TypingService.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FastType.Appdata
 {
@@ -23,13 +26,32 @@ namespace FastType.Appdata
 
         private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
         {
-
+            var button = FindButtonByKey(e.Key);
+            button.BorderThickness = new Thickness(0.0, 0.0, 0.0, 0.0);
         }
 
         private void _typingTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            _typingTextBox.Clear();
-            _typingTextBox.Text = e.Key.ToString();
+            var button = FindButtonByKey(e.Key);
+            if (button != null)
+            {
+                button.BorderThickness = new Thickness(2.0, 2.0, 2.0, 4.0);
+            }
+        }
+
+        private Button FindButtonByKey(Key key)
+        {
+            foreach (var stackPanel in _keyboardGrid.Children.OfType<StackPanel>())
+            {
+                foreach (var button in stackPanel.Children.OfType<Button>())
+                {
+                    if (button.Tag.ToString() == key.ToString())
+                    {
+                        return button;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
