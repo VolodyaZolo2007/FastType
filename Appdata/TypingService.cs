8af691a0e80030ef61482a85dfdff22c87c99f15
Панин.Владимир <v9250578092@gmail.com>
2025-03-24 11:32:25 +0300

    ТАймер

--- Appdata/TypingService.cs
+++ Appdata/TypingService.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -7,10 +8,18 @@ namespace FastType.Appdata
 {
     public class TypingService
     {
+        //Создаем поля для ХРАНИЕНИ и ИСПОЛЬЗОВАНИЕ прерменных для внутренней логики класса
+        private Stopwatch _stopWatch;
+
+        private int _numberOfCharacters;
+        private int _ellapsedTimeInMinutes;
+
         //Создаем поля для хранения и использования элементов управления в рамках класса
         private Grid _keyboardGrid;
         private TextBox _typingTextBox;
         private TextBlock _typingTextBlock;
+        //Создаем свойство для ПОЛУЧЕНИЯ и ПРИСВАИВАНИЯ расчетов
+        public int TypeSpeed { get; private set; }
 
         //Создаем конструктор класса для ПРИЕМА элементов управления из интефейса(TypingTutorPage.xaml)
         public TypingService(Grid keyboardGrid, TextBox typingTextBox, TextBlock typingTextBlock)
@@ -22,12 +31,24 @@ namespace FastType.Appdata
 
             _typingTextBox.PreviewKeyDown += _typingTextBox_PreviewKeyDown;
             _typingTextBox.PreviewKeyUp += _typingTextBox_PreviewKeyUp;
+            _typingTextBox.TextChanged += _typingTextBox_TextChanged;
+        }
+
+        private void _typingTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_typingTextBox.Text.Length >= 1 && !_stopWatch.IsRunning)
+            {
+
+            }
         }
 
         private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
         {
             var button = FindButtonByKey(e.Key);
-            button.BorderThickness = new Thickness(0.0, 0.0, 0.0, 0.0);
+
+
+            button.BorderThickness = new Thickness(0.0);
+
         }
 
         private void _typingTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
